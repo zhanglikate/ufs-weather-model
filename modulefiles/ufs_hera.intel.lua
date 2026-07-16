2547f599156bdb80b53b45bb70f72c6669a73060
Kate.Zhang <Kate.Zhang@noaa.gov>
2026-07-16 17:33:39 -0600

    Update as UFS-Chem

--- modulefiles/ufs_hera.intel.lua
+++ modulefiles/ufs_hera.intel.lua
@@ -2,7 +2,7 @@ help([[
 loads UFS Model prerequisites for Hera/Intel
 ]])
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.1/envs/unified-env-rocky8/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.6.0/envs/fms-2024.01/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
 load(pathJoin("stack-intel", stack_intel_ver))
@@ -10,7 +10,7 @@ load(pathJoin("stack-intel", stack_intel_ver))
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
-cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+cmake_ver=os.getenv("cmake_ver") or "3.28.1"
 load(pathJoin("cmake", cmake_ver))
 
 load("ufs_common")
